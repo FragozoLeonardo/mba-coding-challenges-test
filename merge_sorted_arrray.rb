780fd0ed750473ddd51343b9f9f80a0d42b24a2a
FragozoLeonardo <leo.fragozo.dev@gmail.com>
2025-09-08 16:08:28 -0300

    Exercise #88 done

---
+++ merge_sorted_arrray.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+
+# Exercise number 88
+def merge(nums1, m, nums2, n)
+  i = m - 1
+  j = n - 1
+  k = m + n - 1
+
+  while j >= 0
+    if i >= 0 && nums1[i] > nums2[j]
+      nums1[k] = nums1[i]
+      i -= 1
+    else
+      nums1[k] = nums2[j]
+      j -= 1
+    end
+    k -= 1
+  end
+end
+
+# Exemplos de uso
+nums1 = [1,2,3,0,0,0]
+merge(nums1, 3, [2,5,6], 3)
+puts nums1.inspect  # => [1,2,2,3,5,6]
+
+nums1 = [1]
+merge(nums1, 1, [], 0)
+puts nums1.inspect  # => [1]
+
+nums1 = [0]
+merge(nums1, 0, [1], 1)
+puts nums1.inspect  # => [1]
