d1890e42bc94fe77b95036e0f58ae37d6191c6c4
FragozoLeonardo <leo.fragozo.dev@gmail.com>
2025-09-10 16:16:32 -0300

    Exercise #125 done

---
+++ valid_palindrome.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+#
+# Returns true if s is a palindrome considering only alphanumeric characters and ignoring case.
+# Exercise number 125 - https://leetcode.com/problems/valid-palindrome/
+def is_palindrome(s)
+  i = 0
+  j = s.length - 1
+
+  while i < j
+    # move left pointer to the next alphanumeric char
+    if (s[i] =~ /[A-Za-z0-9]/).nil?
+      i += 1
+      next
+    end
+
+    # move right pointer to the previous alphanumeric char
+    if (s[j] =~ /[A-Za-z0-9]/).nil?
+      j -= 1
+      next
+    end
+
+    # compare characters case-insensitively
+    return false if s[i].downcase != s[j].downcase
+
+    i += 1
+    j -= 1
+  end
+
+  true
+end
