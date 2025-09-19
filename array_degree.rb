c7181ff0e48265a99c31f80af1f545b29ebdfcca
FragozoLeonardo <leo.fragozo.dev@gmail.com>
2025-09-19 16:52:21 -0300

    Exercise 697 - done

---
+++ array_degree.rb
@@ -0,0 +1,27 @@
+# Exercise number 697 - https://leetcode.com/problems/degree-of-an-array/
+
+# @param {Integer[]} nums
+# @return {Integer}
+def find_shortest_sub_array(nums)
+  counts = Hash.new(0)
+  first_index = {}
+  last_index = {}
+
+  nums.each_with_index do |num, i|
+    counts[num] += 1
+    first_index[num] ||= i
+    last_index[num] = i
+  end
+
+  degree = counts.values.max
+
+  min_length = Float::INFINITY
+  counts.each do |num, freq|
+    if freq == degree
+      length = last_index[num] - first_index[num] + 1
+      min_length = [min_length, length].min
+    end
+  end
+
+  min_length
+end
