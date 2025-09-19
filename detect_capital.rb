d3cc2cbe774ae2851de528638ed68fb44f506db6
FragozoLeonardo <leo.fragozo.dev@gmail.com>
2025-09-19 16:09:49 -0300

    Exercise 520 - done

---
+++ detect_capital.rb
@@ -0,0 +1,17 @@
+# Exercise 520 - https://leetcode.com/problems/detect-capital/
+
+# frozen_string_literal: true
+
+# @param {String} word
+# @return {Boolean}
+def detect_capital_use(word)
+  word == word.upcase ||         # todas maiúsculas
+    word == word.downcase ||     # todas minúsculas
+    word == word.capitalize      # apenas a primeira maiúscula
+end
+
+# Exemplos de uso
+p detect_capital_use('Bra') # => true
+p detect_capital_use('leetcode') # => true
+p detect_capital_use('Google')  # => true
+p detect_capital_use('FlaG')    # => false
