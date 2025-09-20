6873670053054d0aa452aa3d0fdf844130354050
FragozoLeonardo <leo.fragozo.dev@gmail.com>
2025-09-20 19:34:24 -0300

    Exercise 155 - done

---
+++ min_stack.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Exercise 155 - https://leetcode.com/problems/min-stack/
+
+class MinStack
+  def initialize
+    @stack = []
+    @min_stack = []
+  end
+
+  # O(1)
+  def push(val)
+    @stack << val
+    return unless @min_stack.empty? || val <= @min_stack[-1]
+
+    @min_stack << val
+  end
+
+  # O(1)
+  def pop
+    val = @stack.pop
+    return unless val == @min_stack[-1]
+
+    @min_stack.pop
+  end
+
+  # O(1)
+  def top
+    @stack[-1]
+  end
+
+  # O(1)
+  def get_min
+    @min_stack[-1]
+  end
+end
